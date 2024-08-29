d9a69f2ff3516e076425df8caf27f6aeb54f9945
raseljulyservices <rasel@julyservices.com>
2024-08-29 16:56:16 +0600

    added openapi-generator-cli

--- projects/shell/src/app/home/home.component.ts
+++ projects/shell/src/app/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { TranslatePipe } from '../pipes/translate.pipe';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [TranslatePipe],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
